d1b4169eabdc089f52edd47a297363ef0469d282
BuildTools <neekthomson@gmail.com>
2017-10-16 12:50:15 -0400

    Add css styling to website using the skeleton CSS framework, add file uploading

--- website.py
+++ website.py
@@ -4,10 +4,12 @@ a mystic door website. The pages that make up the website
 are taken from /templates
 '''
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 website = Blueprint("simple_page", __name__)
 
-@website.route("/")
+@website.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == 'POST':
+        return "Adventure XML has been received"
     return render_template("index.html")
