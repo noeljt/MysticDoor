4973aaf6174bc8a3fdb7e264249a4de724be7d01
david0223 <david0223@comcast.net>
2017-10-26 22:07:54 -0400

    parser function for places and test file

---
+++ XML/xml_parser.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+def parsexml(file):
+	tree = ET.parse(file)
+	root = tree.getroot()
+	r = []
+	for room in root.findall('room'):
+		t = {}
+		t['id'] = room.get('id')
+		t['desc'] = room.find('desc').text
+		if room.find('goal').text == 'true':
+			t['goal'] = True
+		else:
+			t['goal'] = False
+		if room.find('north').text == None:
+			t['north'] = "-1"
+		else:
+			t['north'] = room.find('north').text
+		if room.find('east').text == None:
+			t['east'] = "-1"
+		else:
+			t['east'] = room.find('east').text
+		if room.find('south').text == None:
+			t['south'] = "-1"
+		else:
+			t['south'] = room.find('south').text
+		if room.find('west').text == None:
+			t['west'] = "-1"
+		else:
+			t['west'] = room.find('west').text
+
+		r.append(t)
+	return r
+
