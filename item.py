1792439f00a2850a3a778f166a42d9510b4ecf8a
todarj <johnna.todaro@gmail.com>
2017-11-13 21:36:09 -0500

    Update item.py

--- item.py
+++ item.py
@@ -3,6 +3,7 @@ class Item(object):
         self.id = int(data["id"])
         self.name = data["name"]
         self.desc = data["desc"]
+        self.action = data["action"]
 
     def getID(self):
         return self.id
@@ -13,6 +14,10 @@ class Item(object):
     def getDescription(self):
         return self.desc
 
+    def chooseAction(self):
+        #choose random action
+        return self.action[0]
+    
     # returns a dictionary of Item properties - JSON serializable
     def export(self):
         return self.__dict__
